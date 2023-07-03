a79c0929f38a78ba9b8ace7e68b99927cb6e5c7e
Jordan Wilke <wilke18@mit.edu>
2023-07-03 12:08:06 +0200

    Adding main python files

---
+++ model_input.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+from typing import Optional
+from dataclasses import dataclass
+from collections import namedtuple
+import json
+
+@dataclass
+class ModelInputParameters:
+    model_path_or_name: str
+    name: Optional[str] = str(int(datetime.now().timestamp()))
+    batch_size: Optional[int] = 8
+    per_device_batch_size: Optional[int] = 8
+    pooling_mode: Optional[str] = 'mean'
+    model_output_dir: Optional[str] = './wav2vec2-xlsr'
+    speaker_independent_scenario: Optional[bool] = True
+    eval_steps: Optional[int] = 10
+    logging_steps: Optional[int] = 10
+    input_column: Optional[str] = 'path'
+    output_column: Optional[str] = 'emotion'
+    is_regression: Optional[bool] = False
+    train_test_split: Optional[float] = .8
+    seed: Optional[int] = 0
+
+    def fromJSON(path):
+        with open(path) as f:
+            input_dic = json.load(f)
+            return ModelInputParameters(**input_dic)
+
+
